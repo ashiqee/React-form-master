8babbe0889805b3ae9eeef437d7a38278d6e3ffa
ashiq73 <ashiq.buet73@gmail.com>
2023-09-21 00:07:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import RefForm from "./components/RefForm/RefForm";
+import SimpleForm from "./components/SimpleForm/SimpleForm";
+import StateFulForm from "./components/SimpleForm/StateFullForm/StateFulForm";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      {/* <SimpleForm /> */}
+      {/* <StateFulForm /> */}
+      <RefForm />
+    </>
+  );
+}
+
+export default App;
---
+++ src/_form_.js
@@ -0,0 +1,12 @@
+/*
+ *1. use form tag and onSubmit handler with event (e)
+ *From e access e.target.[name of input field].value
+ *
+ *
+ * 2.controlled element: use individual field state for each input field
+ * *
+ * 3.controlled element: one object holding in a state holding all the input fields value
+ *
+ * 4. uncontrolled element:useRef to create a reference to the element and
+ *  access value by using like : nameRef.current.value
+ */
